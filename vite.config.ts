8915f73c0243bc13c0856035edc01fd79bf065a7
dkurganskii <44755635+dkurganskii@users.noreply.github.com>
2025-10-02 06:44:04 -0500

    fix domain

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 
 // Project site at https://coddmaster.github.io/relaksio-site
+
 export default defineConfig({
 plugins: [react()],
-base: '/relaksio-site/',
+base: '/',
 })
